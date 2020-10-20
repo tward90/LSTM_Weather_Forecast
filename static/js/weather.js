b74587f850ccfc3c8a8bf35fee37c304f25b187a
Col D <60669354+tward90@users.noreply.github.com>
2020-10-19 22:19:36 -0500

    modified weather.js

--- static/js/weather.js
+++ static/js/weather.js
@@ -2,19 +2,42 @@
 d3.json("dummy_data.json").then((weatherData) => {
   const data = Object.values(weatherData);
   first_rd = Object.values(weatherData)[0].rain_drizzle
+  const date_string = Object.keys(weatherData)
 
-  console.log(first_rd)
+  date_int = date_string.map(d => parseInt(d))
 
-  d3.select(".viz_display").select(".card-body")
-  .selectAll('p')
-  .data(data)
-  .enter()
-  .append('p')
-  .html(d => `<div> <strong>Temperature</strong>: ${d.temp.toFixed(0)}° <br> <strong>Low Temp</strong>: ${d.min.toFixed(0)}° <br> <strong>High Temp</strong>: ${d.max.toFixed(0)}° <br><strong>Chance of Rain</strong>: ${d.rain_drizzle * 100}%</div>`)
-  .attr("class",  "col-md-2 col-sm-6")
+  date = date_int.map(d => new Date(d))
 
-  d3.select(".viz_display").select("img")
-  .data(data)
+
+
+d3.select(".viz_display").select("img")
   .attr("src",  `${first_rd >=.5 ?"../../templates/Assets/rain.jpg":"../../templates/Assets/sunny.jpg"}`)
 
+d3.select(".viz_display").select(".card-body")
+  .selectAll('div')
+  .data(date)
+  .enter()
+  .append('div')
+  .html(d  => `<p><strong>Date</strong>: ${new Date(d)} </p>`)
+  .attr("class",  "col-md-2 col-sm-6")
+
+d3.select(".viz_display").select(".card-body")
+  .selectAll('p')
+  .data(data)
+  .append('p')
+  .html(d  => `<strong>Temperature</strong>: ${d.temp.toFixed(0)}° <br>
+                  <strong>Low Temp</strong>: ${d.min.toFixed(0)}° <br>
+                  <strong>High Temp</strong>: ${d.max.toFixed(0)}° <br>
+                  <strong>Chance of Rain</strong>: ${d.rain_drizzle * 100}%`)
 });
+
+
+// d3.select(".viz_display").select(".card-body")
+//   .selectAll('div')
+//   .data(data)
+//   .enter()
+//   .append('div')
+//   .html(d  => `<p><strong>Temperature</strong>: ${d.temp.toFixed(0)}° <br> 
+//                   <strong>Low Temp</strong>: ${d.min.toFixed(0)}° <br> 
+//                   <strong>High Temp</strong>: ${d.max.toFixed(0)}° <br>
+//                   <strong>Chance of Rain</strong>: ${d.rain_drizzle * 100}%</p>`)
